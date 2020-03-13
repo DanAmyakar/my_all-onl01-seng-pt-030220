02966596a4db1c5d917c056eec5ad67b1a5b9033
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-13 17:24:22 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,4 +2,17 @@ require 'pry'
 
 def my_all?(collection)
 
+  i = 0
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i = i + 1
+    
+  end
+  
+  if new_collection.include?(false)
+    false
+  else
+    true
+  end
+
 end
